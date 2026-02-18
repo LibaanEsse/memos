fbb109e58071d5b1807aa0c02e871d8f364d0734
LibaanEsse <libaan.esse@gmail.com>
2026-02-18 15:51:35 +0000

    fix acm for_each validation issue

--- terraform/modules/acm/main.tf
+++ terraform/modules/acm/main.tf
@@ -12,18 +12,17 @@ resource "aws_route53_record" "validation" {
   for_each = {
     for dvo in aws_acm_certificate.this.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
-      record = dvo.resource_record_value
       type   = dvo.resource_record_type
+      record = dvo.resource_record_value
     }
   }
 
   allow_overwrite = true
   zone_id         = var.hosted_zone_id
-
-  name    = each.value.name
-  type    = each.value.type
-  ttl     = 60
-  records = [each.value.record]
+  name            = each.value.name
+  type            = each.value.type
+  ttl             = 60
+  records         = [each.value.record]
 }
 
 resource "aws_acm_certificate_validation" "this" {
