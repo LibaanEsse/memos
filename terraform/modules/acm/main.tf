108dfd6f03f419774d3879ef04c80db289c4f250
LibaanEsse <libaan.esse@gmail.com>
2026-02-19 15:02:42 +0000

    Fix ACM module and update Terraform plan workflow

--- terraform/modules/acm/main.tf
+++ terraform/modules/acm/main.tf
@@ -1,4 +1,4 @@
-# Create ACM Certificate
+# ACM certificate
 resource "aws_acm_certificate" "this" {
   domain_name               = var.domain_name
   subject_alternative_names = var.subject_alternative_names
@@ -9,29 +9,39 @@ resource "aws_acm_certificate" "this" {
   }
 }
 
-# Create Route53 validation records for ACM
-resource "aws_route53_record" "validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.this.domain_validation_options : dvo.domain_name => {
+# Convert domain_validation_options set to a list
+locals {
+  dns_records = [
+    for dvo in aws_acm_certificate.this.domain_validation_options : {
       name   = dvo.resource_record_name
       type   = dvo.resource_record_type
       record = dvo.resource_record_value
     }
+  ]
+}
+
+#  Route53 validation record
+resource "aws_route53_record" "validation" {
+  for_each = {
+    for r in local.dns_records : r.name => r
   }
 
+  zone_id = var.hosted_zone_id
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = 60
+  records = [each.value.record]
+
   allow_overwrite = true
-  zone_id         = var.hosted_zone_id
-  name            = each.value.name
-  type            = each.value.type
-  ttl             = 60
-  records         = [each.value.record]
 
   lifecycle {
     ignore_changes = [records]
   }
 }
 
+# ACM certificate validation
 resource "aws_acm_certificate_validation" "this" {
   certificate_arn         = aws_acm_certificate.this.arn
   validation_record_fqdns = [for r in aws_route53_record.validation : r.fqdn]
 }
+
